c7ca70cd5e04a41a607a88655325c4cfe6f7eefb
luxagen <hello@luxagen.com>
2026-03-07 17:21:30 +0000

    DI trait

--- src/invoke.rs
+++ src/invoke.rs
@@ -3,7 +3,9 @@
 
 use std::io::Write;
 use std::process::{Command, Output, Stdio};
+use std::sync::{Arc, RwLock};
 use anyhow::{Context, Result, anyhow};
+use once_cell::sync::Lazy;
 
 pub enum OutputMode {
 	Inherit,
@@ -17,61 +19,120 @@ pub struct CmdResult {
 	pub stderr: Option<Vec<u8>>,
 }
 
+pub trait CommandRunner: Send + Sync {
+	fn run_cmd(&self, dir: &str, cmd: &[&str], mode: OutputMode) -> Result<CmdResult>;
+	fn run_with_stdin_inherited(&self, dir: &str, cmd: &[&str], stdin_bytes: &[u8]) -> Result<i32>;
+}
+
+struct RealCommandRunner;
+
+impl CommandRunner for RealCommandRunner {
+	fn run_cmd(&self, dir: &str, cmd: &[&str], mode: OutputMode) -> Result<CmdResult> {
+		if cmd.is_empty() {
+			return Err(anyhow!("No command specified"));
+		}
+
+		let program = cmd[0];
+		let arguments = &cmd[1..];
+
+		match mode {
+			OutputMode::Inherit => {
+				let status = Command::new(program)
+					.args(arguments)
+					.current_dir(dir)
+					.stdin(Stdio::inherit())
+					.stdout(Stdio::inherit())
+					.stderr(Stdio::inherit())
+					.status()?;
+
+				Ok(CmdResult {
+					exit_code: status.code().unwrap_or(-1),
+					stdout: None,
+					stderr: None,
+				})
+			},
+			OutputMode::Capture => {
+				let output = Command::new(program)
+					.args(arguments)
+					.current_dir(dir)
+					.stdin(Stdio::null())
+					.stdout(Stdio::piped())
+					.stderr(Stdio::piped())
+					.output()?;
+
+				Ok(CmdResult {
+					exit_code: output.status.code().unwrap_or(-1),
+					stdout: Some(output.stdout),
+					stderr: Some(output.stderr),
+				})
+			},
+			OutputMode::Silent => {
+				let output = Command::new(program)
+					.args(arguments)
+					.current_dir(dir)
+					.stdin(Stdio::null())
+					.stdout(Stdio::null())
+					.stderr(Stdio::null())
+					.output()?;
+
+				Ok(CmdResult {
+					exit_code: output.status.code().unwrap_or(-1),
+					stdout: None,
+					stderr: None,
+				})
+			},
+		}
+	}
+
+	fn run_with_stdin_inherited(&self, dir: &str, cmd: &[&str], stdin_bytes: &[u8]) -> Result<i32> {
+		if cmd.is_empty() {
+			return Err(anyhow!("No command specified"));
+		}
+
+		let program = cmd[0];
+		let arguments = &cmd[1..];
+
+		let mut child = Command::new(program)
+			.args(arguments)
+			.current_dir(dir)
+			.stdin(Stdio::piped())
+			.stdout(Stdio::inherit())
+			.stderr(Stdio::inherit())
+			.spawn()
+			.with_context(|| format!("Failed to spawn command in {}: {:?}", dir, cmd))?;
+
+		if let Some(mut stdin) = child.stdin.take() {
+			stdin.write_all(stdin_bytes)?;
+		}
+
+		let status = child.wait()?;
+		Ok(status.code().unwrap_or(-1))
+	}
+}
+
+static COMMAND_RUNNER: Lazy<RwLock<Arc<dyn CommandRunner>>> = Lazy::new(|| RwLock::new(Arc::new(RealCommandRunner)));
+
+pub struct CommandRunnerGuard {
+	previous: Arc<dyn CommandRunner>,
+}
+
+impl Drop for CommandRunnerGuard {
+	fn drop(&mut self) {
+		let mut runner = COMMAND_RUNNER.write().expect("COMMAND_RUNNER poisoned");
+		*runner = self.previous.clone();
+	}
+}
+
+pub fn set_command_runner_for_test(runner: Arc<dyn CommandRunner>) -> CommandRunnerGuard {
+	let mut slot = COMMAND_RUNNER.write().expect("COMMAND_RUNNER poisoned");
+	let previous = slot.clone();
+	*slot = runner;
+	CommandRunnerGuard { previous }
+}
+
 pub fn run_cmd(dir: &str, cmd: &[&str], mode: OutputMode) -> Result<CmdResult> {
-	if cmd.is_empty() {
-		return Err(anyhow!("No command specified"));
-	}
-
-	let program = cmd[0];
-	let arguments = &cmd[1..];
-
-	match mode {
-		OutputMode::Inherit => {
-			let status = Command::new(program)
-				.args(arguments)
-				.current_dir(dir)
-				.stdin(Stdio::inherit())
-				.stdout(Stdio::inherit())
-				.stderr(Stdio::inherit())
-				.status()?;
-
-			Ok(CmdResult {
-				exit_code: status.code().unwrap_or(-1),
-				stdout: None,
-				stderr: None,
-			})
-		},
-		OutputMode::Capture => {
-			let output = Command::new(program)
-				.args(arguments)
-				.current_dir(dir)
-				.stdin(Stdio::null())
-				.stdout(Stdio::piped())
-				.stderr(Stdio::piped())
-				.output()?;
-
-			Ok(CmdResult {
-				exit_code: output.status.code().unwrap_or(-1),
-				stdout: Some(output.stdout),
-				stderr: Some(output.stderr),
-			})
-		},
-		OutputMode::Silent => {
-			let output = Command::new(program)
-				.args(arguments)
-				.current_dir(dir)
-				.stdin(Stdio::null())
-				.stdout(Stdio::null())
-				.stderr(Stdio::null())
-				.output()?;
-
-			Ok(CmdResult {
-				exit_code: output.status.code().unwrap_or(-1),
-				stdout: None,
-				stderr: None,
-			})
-		},
-	}
+	let runner = COMMAND_RUNNER.read().expect("COMMAND_RUNNER poisoned");
+	runner.run_cmd(dir, cmd, mode)
 }
 
 pub struct CapturedOutput {
@@ -117,28 +178,8 @@ pub fn run_in_dir_status(dir: &str, cmd: &[&str]) -> Result<i32> {
 
 
 pub fn run_with_stdin_inherited(dir: &str, cmd: &[&str], stdin_bytes: &[u8]) -> Result<i32> {
-	if cmd.is_empty() {
-		return Err(anyhow!("No command specified"));
-	}
-
-	let program = cmd[0];
-	let arguments = &cmd[1..];
-
-	let mut child = Command::new(program)
-		.args(arguments)
-		.current_dir(dir)
-		.stdin(Stdio::piped())
-		.stdout(Stdio::inherit())
-		.stderr(Stdio::inherit())
-		.spawn()
-		.with_context(|| format!("Failed to spawn command in {}: {:?}", dir, cmd))?;
-
-	if let Some(mut stdin) = child.stdin.take() {
-		stdin.write_all(stdin_bytes)?;
-	}
-
-	let status = child.wait()?;
-	Ok(status.code().unwrap_or(-1))
+	let runner = COMMAND_RUNNER.read().expect("COMMAND_RUNNER poisoned");
+	runner.run_with_stdin_inherited(dir, cmd, stdin_bytes)
 }
 
 pub fn run_in_dir_capture(dir: &str, cmd: &[&str]) -> Result<CapturedOutput> {
